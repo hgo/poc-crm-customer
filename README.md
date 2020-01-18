403bf5e35d6f9512bfc1dacb1b89f9f5d1968587
tugrul <guven.ozyurt@consultant.turkcell.com.tr>
2020-01-18 14:15:42 +0300

    test properties h2 / env var for default properties

--- README.md
+++ README.md
@@ -8,4 +8,4 @@
 
 
 ## run container
-`docker container run -d -p 8080:8080 poc-customer`
+`docker container run --env DB_URL=jdbc:postgresql://pocpostgres.ctbgumrcydw1.us-east-1.rds.amazonaws.com:5432/postgres --env DB_USER=postgres --env DB_PASS=poc12345 -d -p 8080:8080 poc-customer`
--- src/test/java/tr/com/turkcell/crm/customer/CustomerApplicationTests.java
+++ src/test/java/tr/com/turkcell/crm/customer/CustomerApplicationTests.java
@@ -1,15 +1,28 @@
 package tr.com.turkcell.crm.customer;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class CustomerApplicationTests
 {
+    final static Logger logger = LoggerFactory.getLogger(CustomerApplicationTests.class);
+
+    @Autowired
+    ApplicationContext applicationContext;
 
     @Test
     void contextLoads()
     {
+
+        final String url = applicationContext.getEnvironment()
+                .getProperty("spring.datasource.url");
+
+        logger.info("url : {}", url);
     }
 
 }
